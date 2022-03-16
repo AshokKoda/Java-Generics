ca5aba20a9ed26bfdce9c330a7d73419ed417c45
Ashok Koda <kodaashok60@gmail.com>
2022-03-16 18:33:16 +0530

    Added:UC4_More than three parameters optionals

---
+++ src/main/java/com/bridgelabz/javagenerics/MaximumMoreThan3Parameters.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.javagenerics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaximumMoreThan3Parameters <T extends Comparable<T>> {
+
+	public static <T extends Comparable<T>> T findMax(List<T> arr) {
+
+		T max = arr.get(0);
+		for (T key : arr) {
+			if (key.compareTo(max) > 0)
+				max = key;
+		}
+		return max;
+	}
+	
+	public static void main(String[] args) {
+		MaximumMoreThan3Parameters obj = new MaximumMoreThan3Parameters();
+		System.out.println("The Maximum integer num is:" + obj.findMax(Arrays.asList(4, 7, 8, 9, 44, 55)));
+		System.out.println("The Maximum float num is:" + obj.findMax(Arrays.asList(1.9, 3.0, 6.7, 2.6, 4.5, 5.8)));
+		System.out.println(
+				"The Maximum String  is:" + obj.findMax(Arrays.asList("Apple", "banana", "peach", "mango", "kiwi")));
+	}
+}
